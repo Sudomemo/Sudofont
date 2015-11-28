81b1ddc358b97d3a899cdfb161d9273322bdcfbd
James <jamesdaniel2197@gmail.com>
2015-11-28 12:39:07 +0000

    Revert "Revert "Major changes; now using IcoMoon to fix errors""
    This reverts commit fa1f458226875cebce8c8412291b03fff66b5944.

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,47 @@
 var gulp =        require('gulp');
 var rename =      require('gulp-rename');
 var sketch =      require('gulp-sketch');
-var iconfont =    require('gulp-iconfont');
 var minifyimg =   require('gulp-imagemin');
 var consolidate = require('gulp-consolidate');
+var fs = require('fs');
 
 var filepaths = {
-    sketchPath: 'sudofont.sketch',
-    scssTemplatePath: 'templates/sudofont-template.scss',
-    scssPath: 'build/scss/',
-    fontPath: 'build/fonts/',
-    relFontPath: '../fonts/'
+  fontJSON: 'sudofont.json',
+  sketchPath: 'sudofont.sketch',
+  scssTemplatePath: 'templates/template.scss',
+  scssPath: 'build/scss/',
+  svgPath: 'build/svg/',
+  fontPath: 'build/fonts/',
+  relFontPath: '../fonts/'
 };
 
-// gulp font
-gulp.task('font', function() {
-	// Sketch file goes in
-	gulp.src(filepaths.sketchPath)
-		// Export each sketch artboard to svg
-		.pipe(sketch({
-			export: 'artboards',
-			formats: 'svg'
-		}))
-		// Minify each svg image
-		.pipe(minifyimg())
-		// Build svgs into a font
-		.pipe(iconfont({
-			fontName: 'sudofont',
-			fontHeight: 256, // (this is the Sketch artboard height)
-			descent: 32,
-			formats: ['ttf', 'eot', 'woff', 'woff2'],
-		}))
-		// When the font is done, make the .scss file
-		.on('glyphs', function(glyphs, options) {
-			gulp.src(filepaths.scssTemplatePath)
-				.pipe(consolidate('lodash', {
-					glyphs: glyphs,
-					fontName: 'sudofont',
-					fontPath: filepaths.relFontPath,
-					className: 'sf'
-				}))
-				.pipe(rename({basename:'sudofont'}))
-				.pipe(gulp.dest(filepaths.scssPath))
-		})
-		// Font comes out
-		.pipe(gulp.dest(filepaths.fontPath))
+gulp.task('font:svg', function() {
+  // Sketch file goes in
+  gulp.src(filepaths.sketchPath)
+    // Export each sketch artboard to svg
+    .pipe(sketch({
+      export: 'artboards',
+      formats: 'svg'
+    }))
+    // Minify each svg image
+    .pipe(minifyimg())
+    // Export to svg
+    .pipe(gulp.dest(filepaths.svgPath))
 });
+
+gulp.task('font:scss', function() {
+  // Read the JSOn from IcoMoon
+  var font = JSON.parse(fs.readFileSync(filepaths.fontJSON, 'utf8'));
+  // Compile the template based on the data
+  return gulp.src(filepaths.scssTemplatePath)
+    .pipe(consolidate('lodash', {
+      fontPath: filepaths.relFontPath,
+      name: font.metadata.name,
+      icons: font.iconSets[0].selection,
+      prefix: font.preferences.fontPref.prefix,
+      classSelector: font.preferences.fontPref.classSelector
+    }))
+    .pipe(rename({basename:font.metadata.name}))
+    .pipe(gulp.dest(filepaths.scssPath))
+});
+
