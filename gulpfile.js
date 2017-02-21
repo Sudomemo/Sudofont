1b629e22f6a132ee3d5f794d78cc7df755b64695
James <jamesdaniel2197@gmail.com>
2017-02-21 12:30:41 +0000

    generate unicode range + improve scss template

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const svgo =        require("gulp-svgo");
 const iconfont =    require("gulp-iconfont");
 const consolidate = require("gulp-consolidate");
 const fs = require("fs");
+var _ = require('lodash');
 
 let paths = {
   sketch: "src/sudofont.sketch",
@@ -21,7 +22,7 @@ let font = {
   className: "sf",
 };
 
-gulp.task("font:svg", () => {
+gulp.task("build", () => {
   // Sketch file goes in
   gulp.src(paths.sketch)
     // Export each sketch artboard to svg
@@ -42,13 +43,61 @@ gulp.task("font:svg", () => {
       descent: 40,
     }))
     .on("glyphs", (glyphs) => {
+
+      glyphs = glyphs.map((glyph) => {
+        let codepoint = glyph.unicode[0].charCodeAt(0);
+        return {
+          name: glyph.name,
+          codepoint: codepoint,
+        }
+      });
+
+      let unicodeRange = glyphs.reduce((stack, glyph) => {
+        // get the codepoint for the current item
+        let codepoint = glyph.codepoint;
+        // let most recent segment in the stack
+        let segment = _.last(stack);
+        let segmentIndex = _.lastIndexOf(stack, segment);
+        // get the start and end values of the segment, if present
+        let segmentStart = _.first(segment);
+        let segmentEnd = _.last(segment);
+        // if the segment has no start value, then we just push the current codepoint to it
+        if (!segmentStart) {
+          stack[segmentIndex].push(codepoint);
+        }
+        // if there's no segment end, and the current codepoint is just one value above the segment start, add it to the segment
+        else if ((!segmentEnd) && (codepoint === segmentStart + 1)) {
+          stack[segmentIndex].push(codepoint);
+        }
+        // same as the above, but if the IS already a segment end, we wanna replace it
+        else if ((segmentEnd) && (codepoint === segmentEnd + 1)) {
+          stack[segmentIndex][1] = codepoint;
+        }
+        // if the codepoint is more than 1 value above the segment end, start a new segment
+        else if (codepoint > segmentEnd + 1) {
+          stack.push([codepoint]);
+        }
+        return stack;
+      }, [[]])
+      // at this point, the result will be an array of range segments, like:
+      // [[53340, 53360], [53365, 53370], [53370]]
+      // loop through each segment
+      .map((segment) => {
+        // start the segment with "U+"
+        return "U+" + segment.map((codepoint) => {
+          // convert each segment codepoint to hex
+          return codepoint.toString(16).toUpperCase()
+        // convert the segment to a string, joining the segment start / end codepoints with "-"
+        }).join("-");
+      // covert the segment stack to a string, joining each segment with ", "
+      }).join(", ")
+
       const options = {
         className: font.className,
         fontName: font.name,
         fontPath: paths.fontCSS,
-        glyphs: glyphs.map((glyph) => {
-          return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
-        })
+        unicodeRange: unicodeRange,
+        glyphs: glyphs
       };
 
       gulp.src("src/templates/template.scss")
@@ -58,20 +107,4 @@ gulp.task("font:svg", () => {
 
     })
     .pipe(gulp.dest(paths.fontOut));
-});
-
-gulp.task("font:scss", function() {
-  // Read the JSOn from IcoMoon
-  var font = JSON.parse(fs.readFileSync(filepaths.fontJSON, "utf8"));
-  // Compile the template based on the data
-  return gulp.src(filepaths.scssTemplatePath)
-    .pipe(consolidate("lodash", {
-      fontPath: filepaths.relFontPath,
-      name: font.metadata.name,
-      icons: font.iconSets[0].selection,
-      prefix: font.preferences.fontPref.prefix,
-      classSelector: font.preferences.fontPref.classSelector
-    }))
-    .pipe(rename({basename:font.metadata.name}))
-    .pipe(gulp.dest(filepaths.scssPath))
 });
