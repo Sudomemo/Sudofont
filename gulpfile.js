f780e87d6a37b462d445662fc893f2d2f5b980e6
James <jamesdaniel2197@gmail.com>
2017-02-24 11:39:51 +0000

    improve the unicode-range generator

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const svgo =        require("gulp-svgo");
 const iconfont =    require("gulp-iconfont");
 const consolidate = require("gulp-consolidate");
 const fs = require("fs");
-var _ = require('lodash');
 
 let paths = {
   sketch: "src/sudofont.sketch",
@@ -21,6 +20,22 @@ let font = {
   formats: ["ttf", "eot", "woff", "woff2", "svg"],
   className: "sf",
 };
+
+function getUnicodeRange(array) {
+  var ranges = [], rstart, rend;
+  for (var i = 0; i < array.length; i++) {
+    rstart = array[i];
+    rend = rstart;
+    while (array[i + 1] - array[i] == 1) {
+      rend = array[i + 1];
+      i++;
+    }
+    let starthex = rstart.toString(16).toUpperCase();
+    let endhex = rend.toString(16).toUpperCase();
+    ranges.push(rstart == rend ? "U+" + starthex + "" : "U+" + starthex + "-" + endhex);
+  }
+  return ranges.join(", ");
+}
 
 gulp.task("build", () => {
   // Sketch file goes in
@@ -50,53 +65,20 @@ gulp.task("build", () => {
           name: glyph.name,
           codepoint: codepoint,
         }
+      })
+      .sort((a, b) => {
+        return a.codepoint - b.codepoint;
       });
 
-      let unicodeRange = glyphs.reduce((stack, glyph) => {
-        // get the codepoint for the current item
-        let codepoint = glyph.codepoint;
-        // let most recent segment in the stack
-        let segment = _.last(stack);
-        let segmentIndex = _.lastIndexOf(stack, segment);
-        // get the start and end values of the segment, if present
-        let segmentStart = _.first(segment);
-        let segmentEnd = _.last(segment);
-        // if the segment has no start value, then we just push the current codepoint to it
-        if (!segmentStart) {
-          stack[segmentIndex].push(codepoint);
-        }
-        // if there's no segment end, and the current codepoint is just one value above the segment start, add it to the segment
-        else if ((!segmentEnd) && (codepoint === segmentStart + 1)) {
-          stack[segmentIndex].push(codepoint);
-        }
-        // same as the above, but if the IS already a segment end, we wanna replace it
-        else if ((segmentEnd) && (codepoint === segmentEnd + 1)) {
-          stack[segmentIndex][1] = codepoint;
-        }
-        // if the codepoint is more than 1 value above the segment end, start a new segment
-        else if (codepoint > segmentEnd + 1) {
-          stack.push([codepoint]);
-        }
-        return stack;
-      }, [[]])
-      // at this point, the result will be an array of range segments, like:
-      // [[53340, 53360], [53365, 53370], [53370]]
-      // loop through each segment
-      .map((segment) => {
-        // start the segment with "U+"
-        return "U+" + segment.map((codepoint) => {
-          // convert each segment codepoint to hex
-          return codepoint.toString(16).toUpperCase()
-        // convert the segment to a string, joining the segment start / end codepoints with "-"
-        }).join("-");
-      // covert the segment stack to a string, joining each segment with ", "
-      }).join(", ")
+      let codepoints = glyphs.map((glyph) => {
+        return glyph.codepoint;
+      });
 
       const options = {
         className: font.className,
         fontName: font.name,
         fontPath: paths.fontCSS,
-        unicodeRange: unicodeRange,
+        unicodeRange: getUnicodeRange(codepoints),
         glyphs: glyphs
       };
 
